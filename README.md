a0d57808906bc8ac799ba3bc27cd6367bbb479fc
Romain Bazile <gromain.baz@gmail.com>
2019-09-03 18:19:43 +0200

    First release of openmoji-awesome

---
+++ README.md
@@ -0,0 +1,42 @@
+# Openmoji-awesome 💡
+
+**Like Font-Awesome and Twemoji-amazing, but for Openmoji Emojis!** 🌟
+
+It's CSS classes for all your Emoji needs, now with all the Creative Commons goodness!
+
+_Inspired by [twemoji-amazing](https://github.com/SebastianAigner/twemoji-amazing)!_
+
+![openmoji-banner](openmoji-banner.png)
+
+### Usage
+
+#### Basic Usage
+
+`<i class="oma oma-face-with-monocle"></i>`
+
+#### Size Options
+
+Like in Font-Awesome, emoji sizes can be changed via `oma-lg`, `oma-bg`, `oma-2x`, `oma-3x`, `oma-4x`, and `oma-5x`.
+
+#### Finding Emojis
+
+- Openmoji-awesome uses [original emoji json data](https://github.com/hfg-gmuend/openmoji/blob/master/data/openmoji.json) as its source of codepoints and descriptions.
+- To find an emoji of your liking, check out the [openmoji Emoji list](https://openmoji.org/library/). Replace spaces with hyphens to get the class name! (e.g. "man in suit levitating" becomes `oma-man-in-suit-levitating` 🕴)
+
+To use on your website, it is advised to download the svg files here: https://github.com/hfg-gmuend/openmoji/releases/tag/12.0.0 .
+By default, the script will generate a css file that will look into an openmoji folder on your website.
+
+
+#### Running the generator
+
+`./gradlew run` will generate a fresh version of both the openmoji-color-awesome and the openmoji-black-awesome file in the root directory of the project.
+
+You can integrate those in your project to use either color or black and white (or both!) emojis from Openmoji.
+
+### Licenses & Other
+
+License: [MIT](https://mit-license.org/).
+
+Uses CSS snippets from [twemoji-awesome](https://github.com/ellekasai/twemoji-awesome), licensed under [MIT](http://ellekasai.mit-license.org/).
+
+As per the Openmoji repository, the graphics are licensed under the CC-BY 4.0 which has a pretty good guide on [best practices for attribution](https://wiki.creativecommons.org/Best_practices_for_attribution).
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,63 @@
+package io.bazile.openmojiamazing
+
+import com.beust.klaxon.Klaxon
+import java.io.File
+import java.net.URL
+
+fun openmojiColorUrl(unicode: String) = "https://cdn.jsdelivr.net/gh/hfg-gmuend/openmoji@12.0.0/color/svg/$unicode.svg"
+fun openmojiBlackUrl(unicode: String) = "https://cdn.jsdelivr.net/gh/hfg-gmuend/openmoji@12.0.0/black/svg/$unicode.svg"
+
+data class Emoji(val hexcode: String, val annotation: String)
+
+fun main(args: Array<String>) {
+    val preamble = File("preamble.css")
+    val preambleLines = preamble.readLines()
+    val json = URL("https://raw.githubusercontent.com/hfg-gmuend/openmoji/12.0.0/data/openmoji.json").readText()
+    val result = Klaxon().parseArray<Emoji>(json) ?: listOf()
+    val new = result.filter {
+        it.annotation.all { it.isLetter() || it == ' ' || it == '-' }
+    }.map {
+        Emoji(it.hexcode.toUpperCase().split(" ").joinToString("-"), it.annotation.toLowerCase().split(" ").joinToString("-"))
+    }
+    val cssColorClasses = new.map {
+        """
+            .oma-${it.annotation} {
+                background-image: url("${openmojiColorUrl(it.hexcode)}")
+            }
+        """.trimIndent()
+    }
+    val cssBlackClasses = new.map {
+        """
+        .oma-${it.annotation} {
+        background-image: url("${openmojiColorUrl(it.hexcode)}")
+    }
+    """.trimIndent()
+    }
+    println("Generated a total of ${new.count()} emoji CSS classes.")
+    File("openmoji-color-awesome.css").printWriter().use { out ->
+        preambleLines.forEach {
+            out.println(it)
+        }
+        out.println("""
+        
+        /* Generated a total of ${new.count()} emoji CSS classes. */
+        
+        """.trimIndent())
+        cssColorClasses.forEach {
+            out.println(it)
+        }
+    }
+    File("openmoji-black-awesome.css").printWriter().use { out ->
+        preambleLines.forEach {
+            out.println(it)
+        }
+        out.println("""
+        
+        /* Generated a total of ${new.count()} emoji CSS classes. */
+        
+        """.trimIndent())
+        cssBlackClasses.forEach {
+            out.println(it)
+        }
+    }
+}
